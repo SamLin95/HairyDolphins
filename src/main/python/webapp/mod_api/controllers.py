8770b20c8b1ba4d0b7cb56f1bc3105c222696d0f
Dun Huang <hd200410190@gmail.com>
2016-09-16 00:35:51 -0400

    Corrected some logics, added get user route

--- src/main/python/webapp/mod_api/controllers.py
+++ src/main/python/webapp/mod_api/controllers.py
@@ -40,8 +40,62 @@ HTTP_NETWORK_AUTHENTICATION_REQUIRED = 511
 mod_api = Blueprint('api', __name__, url_prefix='/api')
 api = swagger.docs(flask_restful.Api(mod_api), apiVersion=API_VERSION, api_spec_url='/spec')
 
+class User(flask_restful.Resource):
+    "An User"
+
+    def __init__(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('request_fields', type=str, action='append')
+
+        self.parser = parser
+
+    @swagger.operation(
+        summary = "Returns the information of the user with given id",
+        nickname = "Get User",
+        parameters=[
+            {
+              "name": "user_id",
+              "description": "Primary key of the expected user.",
+              "required": True,
+              "allowMultiple": False,
+              "dataType": "integer",
+              "paramType": "path"
+            },
+            {
+              "name": "request_fields",
+              "description": "Names of the fields of the user that are required to be returned",
+              "required": False,
+              "allowMultiple": True,
+              "dataType": "string",
+              "paramType": "query"
+            },
+       ]
+    )
+    def get(self, user_id):
+        args = self.parser.parse_args()
+        entity_query = Entity.query
+
+        if(args['request_fields']):
+            request_fields = tuple(args['request_fields'])
+            entity_schema = EntitySchema(exclude='password', only=request_fields)
+        else:
+            entity_schema = EntitySchema(exclude='password')
+
+        entity = entity_query.get(user_id)
+
+        if(not entity):
+            return {"message" :"User not found"}, HTTP_NOT_FOUND
+
+        try:
+            entity_json = entity_schema.dump(entity).data
+            return entity_json
+        except AttributeError as err:
+            return {"message" : {"request_fields" : format(err)} }, HTTP_BAD_REQUEST
+
+api.add_resource(User, '/users/<int:user_id>')
+
 class Users(flask_restful.Resource):
-    "Users Information"
+    "A list of users"
 
     def __init__(self):
         parser = reqparse.RequestParser()
@@ -53,8 +107,8 @@ class Users(flask_restful.Resource):
         self.parser = parser
 
     @swagger.operation(
-        summary = "Search for users given criteria",
-        nickname = "User Search",
+        summary = "Returns the information of a list of users satisfying given criteria",
+        nickname = "Search Users",
         parameters=[
             {
               "name": "user_id",
@@ -77,7 +131,7 @@ class Users(flask_restful.Resource):
               "description": "Names of the fields of each user that are required to be returned",
               "required": False,
               "allowMultiple": True,
-              "dataType": "integer",
+              "dataType": "string",
               "paramType": "query"
             },
        ]
@@ -99,7 +153,11 @@ class Users(flask_restful.Resource):
             if(not entity):
                 return {"message" :"User not found"}, HTTP_NOT_FOUND
 
-            entity_json = entity_schema.dump(entity).data
+            try:
+                entity_json = entity_schema.dump(entity).data
+                return entity_json
+            except AttributeError as err:
+                return {"message" : {"request_fields" : format(err)} }, HTTP_BAD_REQUEST
         else:
             if(args['role_id']):
                 role_id = args['role_id']
@@ -116,10 +174,9 @@ class Users(flask_restful.Resource):
 
             try:
                 entity_json = entity_schema.dump(entities, many=True).data
+                return entity_json
             except AttributeError as err:
                 return {"message" : {"request_fields" : format(err)} }, HTTP_BAD_REQUEST
-
-        return entity_json
 
 api.add_resource(Users, '/users')
 
