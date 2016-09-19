2e6735a7ff56f4543d45f4fd4dc693ff2c683290
Dun Huang <hd200410190@gmail.com>
2016-09-19 15:11:29 -0400

    Changed some routes

--- src/main/python/webapp/mod_search/controllers.py
+++ src/main/python/webapp/mod_search/controllers.py
@@ -2,6 +2,10 @@ from flask import Blueprint, request, render_template, redirect, url_for
 
 mod_search = Blueprint('search', __name__, url_prefix='/search')
 
-@mod_search.route('/', methods=['POST', 'GET'])
-def test():
-	return "This is search route"
+@mod_search.route('/laSearch', methods=['POST', 'GET'])
+def searchLocalAdvisors():
+    return render_template('laSearch.html')
+
+@mod_search.route('/rdSearch', methods=['POST', 'GET'])
+def search_recommendations():
+    return render_template('recommendations.html')
