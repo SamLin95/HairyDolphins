32b63ebeaf3a35c33edf3ebffb51b60bcd8869ce
Dun Huang <huangdun@gatech.edu>
2016-10-25 17:52:52 -0400

    removed s3 credentials

--- src/main/python/webapp/lib/s3_lib.py
+++ src/main/python/webapp/lib/s3_lib.py
@@ -5,13 +5,11 @@ import sys
 from boto.s3.key import Key
 
 #TODO: To be stored in separate files
-AWS_ACCESS_KEY_ID = 'AKIAIRNR57AQD5F7EEJA'
-AWS_SECRET_ACCESS_KEY = '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'
 AWS_S3_BUCKET_NAME = 'hairydolphins'
 
 class S3Helper():
     def __init__(self):
-        conn = boto.connect_s3(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY)
+        conn = boto.connect_s3()
         bucket = conn.get_bucket(AWS_S3_BUCKET_NAME)
         self.bucket = bucket
 
