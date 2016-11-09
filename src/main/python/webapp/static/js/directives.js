17d6c61780f05073229e7062e3a300b22bad907c
Dun Huang <huangdun@gatech.edu>
2016-11-09 17:40:49 -0500

    Scroll bottom automatically

--- src/main/python/webapp/static/js/directives.js
+++ src/main/python/webapp/static/js/directives.js
@@ -27,3 +27,21 @@ app.directive('customOnChange', function() {
     }
   };
 });
+
+app.directive('scrollBottom', function ($timeout) {
+  return {
+    scope: {
+      scrollBottom: "="
+    },
+    link: function (scope, element) {
+      scope.$watchCollection('scrollBottom', function (newValue) {
+        if (newValue)
+        {
+          $timeout(function(){
+            $(element).scrollTop($(element)[0].scrollHeight)
+          }, 0)
+        }
+      });
+    }
+  }
+})
